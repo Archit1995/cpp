6a7f56b1224ad87f7431091af0831972d1d2f906
Archit Srivastava <archit.srivastava1995@gmail.com>
2020-06-11 20:20:51 +0530

    First file added. Stores and prints # in a vector.

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,1 +1,18 @@
-//Declaration and init about vectors
+//Declaration and initialization about vectors
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+	vector<int> v;//vector declaration.
+	cout<<"Enter number "<<endl;
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+		v.push_back(i);
+	cout<<"The size of the vector is "<<v.size()<<endl;
+	for(int j=0;j<n;j++)
+		cout<<v.at(j)<<" ";
+
+return 0;
+}
+
